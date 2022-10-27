87d9d8e29ce42be8926d496becc76eb976c058ec
fahadammar <fahadammar96@outlook.com>
2022-10-27 23:57:35 +0500

    The small circle is made visible, The issue is not running circle with countdown timer, need to make it work

--- app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExcerciseActivity.kt
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/ExcerciseActivity.kt
@@ -3,6 +3,7 @@ package eu.tutorials.a7_minutesworkoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import eu.tutorials.a7_minutesworkoutapp.databinding.ActivityExcerciseBinding
 import java.util.*
@@ -13,12 +14,16 @@ class ExcerciseActivity : AppCompatActivity() {
     var excerciseBinding: ActivityExcerciseBinding? = null
 
     // COUNT DOWN VARIABLES
-    private var timer : CountDownTimer? = null
-    private var progressNum = 0
+    private var restTimer : CountDownTimer? = null
+    private var restProgress = 0
 
-
+    // Excercise Timers
+    private var excerciseTimer : CountDownTimer? = null
+    private var excerciseProgress = 0
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // BINDING
         excerciseBinding = ActivityExcerciseBinding.inflate(layoutInflater)
         val view = excerciseBinding!!.root
         setContentView(view)
@@ -43,30 +48,31 @@ class ExcerciseActivity : AppCompatActivity() {
 
     private fun setupProgressView(){
         /*
-         * Here firstly we will check if the timer is running the and it is not null then cancel the running timer and start the new one.
+         * Here firstly we will check if the timer is running the
+         * and it is not null then cancel the running timer and start the new one.
          * And set the progress to initial which is 0.
          */
-        if(timer != null)
+        if(restTimer != null)
         {
-            timer!!.cancel()
-            progressNum = 0
+            restTimer!!.cancel()
+            restProgress = 0
         }
         // This function is which contains the count flow. It's used to set the progress details
-        theProgressBarFunctionality()
+        setupRestView()
     }
 
-    // THE COUNT DOWN TIMER FUNCTION
-    private fun theProgressBarFunctionality(){
-        excerciseBinding!!.progressBar.progress = progressNum
+    // THE COUNT DOWN TIMER FUNCTION - this is named as -->
+    private fun setupRestView(){
+        excerciseBinding!!.progressBar.progress = restProgress
         // Here we have started a timer of 10 seconds so the 10000 is milliseconds is 10 seconds and the countdown interval is 1 second so it 1000.
-        timer = object : CountDownTimer(1000, 1000){
+        restTimer = object : CountDownTimer(1000, 1000){
             override fun onTick(millisUntilFinished: Long) {
                 // Increasing by 1
-                progressNum++
+                restProgress++
                 // Indicates progress bar progress
-                excerciseBinding!!.progressBar.progress = 10 - progressNum
+                excerciseBinding!!.progressBar.progress = 10 - restProgress
                 // The TextView Number in Between the Progress. It's basically the text in terms of seconds
-                excerciseBinding!!.tvTimer.text = (10 - progressNum).toString()
+                excerciseBinding!!.tvTimer.text = (10 - restProgress).toString()
             }
             override fun onFinish() {
                 // When the 10 seconds will complete this will be executed.
@@ -79,14 +85,51 @@ class ExcerciseActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupExcerciseView(){
+        excerciseBinding?.progressBarExcercise?.visibility = View.INVISIBLE
+        excerciseBinding?.tvTitle?.text = "Excercise Name"
+        excerciseBinding?.flExcercise?.visibility = View.VISIBLE
+
+        if(excerciseTimer != null){
+            excerciseTimer?.cancel()
+            excerciseProgress = 0
+        }
+
+        setExcerciseProgressBar()
+    }
+
+    // THE COUNT DOWN TIMER FUNCTION - this is named as -->
+    private fun setExcerciseProgressBar(){
+        excerciseBinding!!.progressBar.progress = restProgress
+        // Here we have started a timer of 10 seconds so the 10000 is milliseconds is 10 seconds and the countdown interval is 1 second so it 1000.
+        restTimer = object : CountDownTimer(1000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                // Increasing by 1
+                excerciseProgress++
+                // Indicates progress bar progress
+                excerciseBinding!!.progressBarExcercise.progress = 30 - excerciseProgress
+                // The TextView Number in Between the Progress. It's basically the text in terms of seconds
+                excerciseBinding!!.tvTimerExcercise.text = (30 - excerciseProgress).toString()
+            }
+            override fun onFinish() {
+                setupExcerciseView()
+            }
+        }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
         excerciseBinding = null
-        if(timer != null)
+        if(restTimer != null)
         {
-            timer!!.cancel()
-            progressNum = 0
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        if(excerciseTimer != null)
+        {
+            excerciseTimer?.cancel()
+            excerciseProgress = 0
         }
     }
 }
